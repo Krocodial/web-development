db4d4dd11efb0d3facd015b100ddb34db19f10a9
Krocodial <xxkraaklxx@gmail.com>
2017-03-03 19:57:32 -0800

    added book models

--- Django/locallibrary/catalog/models.py
+++ Django/locallibrary/catalog/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+	"""
+	Model representing a book genre
+	"""
+	name = models.CharField(max_length=200, help_text="Enter a book genre")
+	
+	def __str__(self):
+	"""
+	String for representing the model object
+	"""
+		return self.name
+
+
+from django.urls import reverse
+
+class Book(models.Model):
+	"""
+	Model representing a book
+	"""
+	title = models.CharField(max_length=200)
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	summary = models.TextField(max_length=1000, help_text="Enter a brief description")
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+	def __str__(self):
+		return self.title
+	
+	def get_absolute_url(self):
+		return reverse('book-detail', args=[str(self.id)])
+	
+
+
+
+
+
+
